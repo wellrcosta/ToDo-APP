97c535a60de9e54b85f4006b661fdc3a79f3040e
Wellington Reis <wellington.costa.97@gmail.com>
2020-03-11 17:56:25 -0300

    Frontend edits

---
+++ frontend/src/AppFooter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class AppFooter extends Component {
+
+    render() {
+        return  (
+            <div className="layout-footer">
+                <span className="footer-text">Wellington Reis</span>
+            </div>
+        );
+    }
+}
--- frontend/src/components/TodoForm.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/button-has-type */
-import React from 'react';
-
-export default function TodoForm() {
-  return <div />;
-}
